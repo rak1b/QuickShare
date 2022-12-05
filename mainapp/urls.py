7bac74644e513890fe82bc13673aaa397f6b3125
rak1b <rak13.dev@gmail.com>
2022-12-06 00:56:19 +0600

    file upload and design added

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.homeview),
+    path('upload/', views.upload_files),
+    path('download/<str:uid>', views.download_files),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse, JsonResponse
+import shutil
 # Create your views here.
+from .models import Folder, Files
+import json
 
 
 def homeview(request):
     return render(request, "main/home.html")
+
+
+def download_files(request, uid):
+    url = {'url': f"{request.scheme }://{ request.META.get('HTTP_HOST')}/static/zip/{uid}.zip"}
+    return render(request, 'main/download.html', context=url)
+
+
+def zip_files(folder):
+    shutil.make_archive(f'static/zip/{folder}', 'zip', f'media/{folder}')
+
+
+def upload_files(request):
+
+    if request.method == 'POST':
+
+        files = request.FILES.values()
+        folder = Folder.objects.create()
+        files_objs = []
+        for file in files:
+            # print("files", file)
+            files_obj = Files.objects.create(folder=folder, file=file)
+            files_objs.append(files_obj)
+
+        zip_files(folder.uid)
+        return JsonResponse({'uid': folder.uid})
+        # return JsonResponse({'url': f"{request.scheme }://{ request.META.get('HTTP_HOST')}/static/zip/{folder.uid}.zip"})
